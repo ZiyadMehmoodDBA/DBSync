745406858bcd5aac1d61ae2abdd8d09f25aa9a55
Ziyad Mehmood <zmehmood@ad.mcmso.co>
2026-06-22 16:32:24 +0500

    fix(api): validate page >= 1 in BatchListRequestValidator

--- src/MSOSync.Api/Validators/BatchListRequestValidator.cs
+++ src/MSOSync.Api/Validators/BatchListRequestValidator.cs
@@ -9,6 +9,9 @@ public sealed class BatchListRequestValidator : AbstractValidator<BatchListReque
 
     public BatchListRequestValidator()
     {
+        RuleFor(x => x.Page)
+            .GreaterThanOrEqualTo(1).WithMessage("page must be >= 1");
+
         RuleFor(x => x.PageSize)
             .InclusiveBetween(1, 100).WithMessage("pageSize must be between 1 and 100");
 
